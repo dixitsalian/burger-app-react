944efa620f6f0a017af84bb3ae7ae815c579fe16
Dixit Salian <dixitsalian43@gmail.com>
2020-08-14 21:17:11 +0530

    routing and form implementation

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -28,6 +28,7 @@ class BurgerBuilder extends Component{
     }
 
     componentDidMount() {
+        console.log(this.props);
         axios.get('https://react-my-burger-e0712.firebaseio.com/ingredients.json')
         .then(  response => { 
             this.setState({ ingredients: response.data });
@@ -74,27 +75,16 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = () => {
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Test A',
-                email: 'test@test.com',
-                address: {
-                    zipcode: '4444',
-                    street: '12 Test road',
-                    country: 'Germany',
-                }
-            },
-            deliveryMethod: 'fastest'
+        const queryParams = [];
+
+        for (let i in this.state.ingredients) {
+            queryParams.push( encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
         }
-        axios.post('/orders.json', order)
-        .then( (response) => {
-            this.setState({loading: false, purchasing: false});
-        })
-        .catch( (error) => {
-            this.setState({loading: false, purchasing: false});
+        queryParams.push('price='+ this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: queryString
         });
     }
 
